28dc7b224078b49be2095ef08aeaae06ed66ba5f
ashmidt <Joujvemkiefjoft3>
2021-12-01 11:37:18 +0300

    remove indb

--- sw.js
+++ sw.js
@@ -1,19 +1,19 @@
-const CACHE = 'uid-cache-v4';
+const CACHE = "uid-cache-v6";
 
-self.addEventListener('install', (event) => {
+self.addEventListener("install", (event) => {
+    console.log("!!!!! install");
+
     event.waitUntil(
-        caches.open(CACHE).then((cache) => cache.addAll([
-                './main.js'
-            ])
-        ));
+        caches.open(CACHE).then((cache) => cache.addAll(["./main.js"]))
+    );
 });
 
-self.addEventListener('activate', (event) => {
-    console.log('Активирован');
+self.addEventListener("activate", (event) => {
+    console.log("Активирован");
 
     caches.open(CACHE).then((cache) => {
         console.log(cache.keys());
-    })
+    });
 });
 
 self.addEventListener("fetch", (event) => {
@@ -22,27 +22,36 @@ self.addEventListener("fetch", (event) => {
 
     const { url } = event.request;
 
-    const uid = new URL(url).searchParams.get('uid');
+    const uid = new URL(url).searchParams.get("uid");
 
     if (!uid) {
         event.respondWith(fetch(event.request));
         return;
     }
 
-    console.log('!!!!! uid', uid);
+    console.log("!!!!! uid", uid);
 
     event.respondWith(
         caches.match(event.request).then((resp) => {
             console.log("!!!!! event.request", resp);
             return (
-                resp || caches.open(CACHE).then((cache) => {
-                                    const body = new FormData();
-                                    body.append('uid', uid)
-                                    const response = new Response(body, {status: 200, ok: true});
-                                    console.log('!!!!! response', response);
-                                    cache.put(event.request, response.clone());
-                                    return response;
-                                })
+                resp ||
+                caches.open(CACHE).then((cache) => {
+                    const options = {
+                        headers: {
+                            "Content-Type": "application/json",
+                        },
+                        status: 200,
+                        ok: true,
+                        url: event.request.url,
+                    };
+
+                    const jsonResponse = new Response(JSON.stringify({ uid }), options);
+
+                    console.log("!!!!! response", jsonResponse);
+                    cache.put(event.request, jsonResponse.clone());
+                    return jsonResponse;
+                })
             );
         })
     );
